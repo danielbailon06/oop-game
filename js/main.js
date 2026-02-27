8a152c057f3783f964547d5b52821cae042dfb48
Daniel Bailón <dani.hd6@gmail.com>
2026-02-27 13:00:19 +0100

    Implemented obstacle movement

--- js/main.js
+++ js/main.js
@@ -1,12 +1,10 @@
 class Player {
     constructor() {
-        this.positionX = 0;
+        this.positionX = 50;
         this.positionY = 0;
         this.width = 10;
         this.height = 10;
-
         this.updateUI();
-
     }
 
     updateUI() {
@@ -32,7 +30,55 @@ class Player {
     }
 }
 
+class Obstacle {
+    constructor() {
+        this.width = 10;
+        this.height = 10;
+        this.positionX = Math.floor(Math.random() * (100 - this.width + 1));
+        this.positionY = 100;
+        this.createDomElement();
+    }
+
+    createDomElement() {
+        this.obstacleElm = document.createElement("div");
+
+        this.obstacleElm.className = "obstacle";
+
+        const parentElm = document.getElementById("board");
+        parentElm.appendChild(this.obstacleElm);
+    }
+
+    updateUI(){
+        this.obstacleElm.style.left = this.positionX + "vw";
+        this.obstacleElm.style.bottom = this.positionY + "vh";
+        this.obstacleElm.style.width = this.width + "vw";
+        this.obstacleElm.style.height = this.height + "vh";
+    }
+
+    moveDown() {
+        this.positionY--;
+        this.updateUI();
+    }
+}
+
 const player = new Player();
+
+const obstacleArr = []
+
+// generar nuevos obstaculos
+
+setInterval(() => {
+    const newObstacle = new Obstacle();
+    obstacleArr.push(newObstacle);
+}, 4000);
+
+// mover todos los obstaculos
+
+setInterval(() => {
+    obstacleArr.forEach(function (obstacleInstance, i, arr) {
+        obstacleInstance.moveDown()
+    })
+}, 30);
 
 document.addEventListener("keydown", (e) => {
     if (e.code === "ArrowLeft") {
